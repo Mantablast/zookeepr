776b2abc1ab6e8a241cf0ac02e3dbba73020e2eb
AimeeJay <ajesso@post.com>
2021-05-01 12:11:52 -0400

    param and query entry

--- server.js
+++ server.js
@@ -3,9 +3,30 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals.json');
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+    res.json(result);
+});
+
 app.get('/api/animals', (req, res) => {
     res.json(animals);
   });
+
+
 
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
